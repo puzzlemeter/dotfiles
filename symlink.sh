6e4cf599a174e10a8a088efe20643047feeb5add
Sonia(Yunghsin-liang) <yunghsin-liang@c-fo.com>
2023-05-08 12:08:42 +0900

    fix: fix settings for m1 and refactor symlink
    fix: fix brew path
    
    chore: alacritty start without tmux attach

--- symlink.sh
+++ symlink.sh
@@ -1,17 +1,20 @@
-ln -sfn $HOME/dotfiles/nvim ./.config/nvim
-ln -sfn $HOME/dotfiles/.alacritty.yml ./.alacritty.yml
-ln -sfn $HOME/dotfiles/.sh_path_and_aliases.d/.public_path ./.public_path
-ln -sfn $HOME/dotfiles/.sh_path_and_aliases.d/.common_alias ./.common_alias
-ln -sfn $HOME/dotfiles/tmux/.tmux.conf ./.tmux.conf
-ln -sfn $HOME/dotfiles/tmux/.tmux.conf.local ./.tmux.conf.local
-ln -sfn $HOME/dotfiles/zsh/.zshrc ./.zshrc
-ln -sfn $HOME/dotfiles/bash/.bash_profile ./.bash_profile
-ln -sfn $HOME/dotfiles/git/.git-completion.bash ./.git-completion.bash
+ln -sfn $HOME/dotfiles/nvim $HOME/.config/nvim
+ln -sfn $HOME/dotfiles/.alacritty.yml $HOME/.alacritty.yml
+ln -sfn $HOME/dotfiles/.sh_path_and_aliases.d/.public_path $HOME/.public_path
+ln -sfn $HOME/dotfiles/.sh_path_and_aliases.d/.common_alias $HOME/.common_alias
+ln -sfn $HOME/dotfiles/tmux/.tmux.conf $HOME/.tmux.conf
+ln -sfn $HOME/dotfiles/tmux/.tmux.conf.local $HOME/.tmux.conf.local
+ln -sfn $HOME/dotfiles/zsh/.zprofile $HOME/.zprofile
+ln -sfn $HOME/dotfiles/zsh/.zshrc $HOME/.zshrc
+# bash profile not really tested
+ln -sfn $HOME/dotfiles/bash/.bash_profile $HOME/.bash_profile
+ln -sfn $HOME/dotfiles/bash/.bashrc $HOME/.bashrc
+ln -sfn $HOME/dotfiles/git/.git-completion.bash $HOME/.git-completion.bash
 # .git-completion.zsh
-ln -sfn $HOME/dotfiles/git/_git ./.zsh/_git
+ln -sfn $HOME/dotfiles/git/_git $HOME/.zsh/_git
 # not in use, use starship instead
 # ln -sfn $HOME/dotfiles/git/.git-prompt.sh ./.git-prompt.sh
-ln -sfn $HOME/dotfiles/git/.gitignore_global ./.gitignore_global
+ln -sfn $HOME/dotfiles/git/.gitignore_global $HOME/.gitignore_global
 
 printf "\033[36mSymlink Completed.\033[0m\n"
 printf "\n"
---
+++ zsh/.zprofile
@@ -0,0 +1,13 @@
+
+is_arm64() {
+  test "$(uname -m)" = "arm64"
+}
+
+if is_arm64; then
+  eval "$($HOME/homebrew/bin/brew shellenv)"
+  local homebrew_bin="$HOME/homebrew/bin"
+  if ! cat /etc/paths | grep -q "${homebrew_bin}"; then
+    echo "setting up homebrew binary path for gui apps"
+    echo -e "${homebrew_bin}\n$(cat /etc/paths)" | sudo tee /etc/paths > /dev/null
+  fi
+fi
