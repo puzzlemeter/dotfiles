a84369a1456cb55184a075ff44186b3de0aabca0
Sonialiang <bingo605@gmail.com>
2024-02-09 23:50:36 +0800

    chore:add keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,4 +1,22 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
 vim.keymap.set("i", "jj", "<ESC>", { silent = true })
+
+vim.keymap.set(
+  "n",
+  "<leader>cf",
+  copy_relative_file_path,
+  { desc = "Copy relative filepath", silent = true, noremap = true }
+)
+
+-- TODO specify the case
+if vim.loop.os_uname().sysname ~= "Darwin" then
+  vim.keymap.set(
+    "n",
+    "<leader>c",
+    require("osc52").copy_operator,
+    { desc = "yank operator", silent = true, expr = true }
+  )
+  vim.keymap.set("n", "yy", "<leader>c_", { desc = "Copy current line", silent = true, remap = true })
+  vim.keymap.set("v", "<leader>c", require("osc52").copy_visual)
+end
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,3 +3,38 @@
 -- Add any additional options here
 local opt = vim.opt
 opt.relativenumber = false
+-- I can still toggle relativenumber using <leader>uL
+
+-- keep mac using pbcopy
+if vim.loop.os_uname().sysname == "Darwin" then
+  vim.g.clipboard = {
+    name = "macOS-clipboard",
+    copy = {
+      ["+"] = "pbcopy",
+      ["*"] = "pbcopy",
+    },
+    paste = {
+      ["+"] = "pbpaste",
+      ["*"] = "pbpaste",
+    },
+    cache_enabled = 0,
+  }
+  vim.g.python_host_prog = "$HOME/.asdf/shims/python"
+  vim.g.python3_host_prog = "$HOME/.asdf/shims/python3"
+end
+
+-- copy relative file path
+copy_relative_file_path = function()
+  local absolute_file_path = vim.api.nvim_buf_get_name(0)
+  local git_path = vim.fn.system("git rev-parse --show-toplevel")
+
+  local relative_file_path = "/" .. string.sub(absolute_file_path, git_path:len() + 1)
+  if vim.loop.os_uname().sysname == "Darwin" then
+    local copy_command = "echo " .. '".' .. relative_file_path .. '"' .. " | pbcopy"
+    vim.fn.system(copy_command)
+  else
+    require("osc52").copy(relative_file_path)
+  end
+  print("copied:" .. relative_file_path)
+  return true
+end
